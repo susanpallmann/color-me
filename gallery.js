4aa6321f13558e4e0853d8b3af0c59f45f9c1e7d
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-04-08 18:40:25 +1000

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -5,6 +5,6 @@ window.onload = function() {
     if ($("#gallery div div div").length >= maxThumbs) {
       $("#gallery div div div:last-child").remove();
     }
-    $("#gallery div div.span12").prepend("<div style='display:inline-block; width: 200px; height: 200px; background-color: " + data.val().color + "'>" + data.val().title + "<br><small></small>by " + data.val().creator + "</div>");
+    $("#gallery div div.span12").prepend("<div style='display:inline-block; width: 200px; height: 200px; background-color: " + data.val().color + "'>Color me " + data.val().title + "<br><small></small>by " + data.val().creator + "</div>");
   });
 };
