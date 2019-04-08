496e090c42f2517088c979ea8427a3ccf90d79ba
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-04-08 18:04:58 +1000

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -2,7 +2,7 @@ function loadGallery() {
   var galleryHTML = "";
   firebase.database().ref('perspectives').orderByChild('title').limitToFirst(6).once('value', function(snapshot) {
     snapshot.forEach(function(childSnapshot) {
-      galleryHTML = galleryHTML + "<span style='width: 200px; height: 200px; background-color: " + childSnapshot.val().color + "'>" + childSnapshot.val().title + "<br><small></small>by " + childSnapshot.val().creator + "</span>";
+      galleryHTML = galleryHTML + "<div style='display:inline-block; width: 200px; height: 200px; background-color: " + childSnapshot.val().color + "'>" + childSnapshot.val().title + "<br><small></small>by " + childSnapshot.val().creator + "</span>";
     });
   });
   $("#gallery div div").html(galleryHTML);
@@ -12,6 +12,6 @@ window.onload = function() {
   loadGallery();
   firebase.database().ref('perspectives').on('child_added', function(data) {
     $("#gallery div div span:last-child").remove();
-    $("#gallery div div").prepend("<span style='width: 200px; height: 200px; background-color: " + data.val().color + "'>" + data.val().title + "<br><small></small>by " + data.val().creator + "</span>");
+    $("#gallery div div").prepend("<div style='display:inline-block; width: 200px; height: 200px; background-color: " + data.val().color + "'>" + data.val().title + "<br><small></small>by " + data.val().creator + "</span>");
   });
 };
