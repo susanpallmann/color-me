168c270887989ae3c13d5f788ebefa312f9d9b39
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-04-08 18:31:16 +1000

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -2,8 +2,8 @@ maxThumbs = 6;
 
 window.onload = function() {
   firebase.database().ref('perspectives').orderByChild('timestamp').on('child_added', function(data) {
-    if ($("#gallery div div span").length >= maxThumbs) {
-      $("#gallery div div span:last-child").remove();
+    if ($("#gallery div div div").length >= maxThumbs) {
+      $("#gallery div div div:last-child").remove();
     }
     $("#gallery div div.span12").prepend("<div style='display:inline-block; width: 200px; height: 200px; background-color: " + data.val().color + "'>" + data.val().title + "<br><small></small>by " + data.val().creator + "</div>");
   });
