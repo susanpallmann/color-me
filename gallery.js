e4518cedc379c48d5509d12f80de163e7fbb3afd
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-04-08 18:44:28 +1000

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -2,9 +2,9 @@ maxThumbs = 6;
 
 window.onload = function() {
   firebase.database().ref('perspectives').orderByChild('timestamp').limitToLast(maxThumbs).on('child_added', function(data) {
-    if ($("#gallery div div div").length >= maxThumbs) {
-      $("#gallery div div div:last-child").remove();
+    if ($("#gallery div").length >= maxThumbs) {
+      $("#gallery div:last-child").remove();
     }
-    $("#gallery div div.span12").prepend("<div style='display:inline-block; width: 200px; height: 200px; background-color: " + data.val().color + "'>Color me " + data.val().title + "<small>by " + data.val().creator + "</small></div>");
+    $("#gallery").prepend("<div style='display:inline-block; width: 200px; height: 200px; background-color: " + data.val().color + "'>Color me " + data.val().title + "<small>by " + data.val().creator + "</small></div>");
   });
 };
