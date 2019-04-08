6fd82e9f59a67ceaca8d2290d162106d4f0aed53
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-04-08 20:56:54 +1000

    Create play.js

---
+++ play.js
@@ -0,0 +1,22 @@
+function determinePerspective() {
+  var perspID = "";
+  var currentURL = window.location.href;
+  if (currentURL.includes("?")) {
+    perspID = currentURL.split("?")[1];
+  }
+  return perspID;
+}
+
+function loadPerspective(perspID) {
+    firebase.database().ref('perspectives/' + perspID).once('value').then(function(snapshot) {
+        var title = snapshot.child('title').val();
+        var creator = snapshot.child('creator').val();
+        var color = snapshot.child('color').val();
+        alert(title + " by " + creator + ", theme: " + color);
+    });
+}
+
+window.onload = function() {
+  var perspID = determinePerspective();
+  loadPerspective();
+);
