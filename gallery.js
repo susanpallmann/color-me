e72c36dcdf450945b234995e694c364c064e0101
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-04-08 18:14:27 +1000

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -12,10 +12,10 @@ function loadGallery() {
 
 window.onload = function() {
   loadGallery();
-  firebase.database().ref('perspectives').on('child_added', function(data) {
-    if ($("#gallery div div span").length >= maxThumbs) {
-      $("#gallery div div span:last-child").remove();
-    }
-    $("#gallery div div").prepend("<div style='display:inline-block; width: 200px; height: 200px; background-color: " + data.val().color + "'>" + data.val().title + "<br><small></small>by " + data.val().creator + "</div>");
-  });
+  //firebase.database().ref('perspectives').on('child_added', function(data) {
+  //  if ($("#gallery div div span").length >= maxThumbs) {
+  //    $("#gallery div div span:last-child").remove();
+  //  }
+  //  $("#gallery div div").prepend("<div style='display:inline-block; width: 200px; height: 200px; background-color: " + data.val().color + "'>" + data.val().title + "<br><small></small>by " + data.val().creator + "</div>");
+  //});
 };
