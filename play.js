50a1ef99614851b0b6b5481f64c01eca9041a2de
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-04-12 15:06:56 +1000

    Update play.js

--- play.js
+++ play.js
@@ -17,21 +17,7 @@ function loadPerspective(perspID) {
         var views = snapshot.child('views').val();
         var newViews = Number(views + 1);
         firebase.database().ref('perspectives/' + perspID).set({views: newViews});
-        alert("This experience has " + (views + 1) + " views!");
-        var compCol = getComplementaryColor(hue, sat, lgh);
-        var compHue = compCol[0];
-        var compSat = compCol[1];
-        var compLgh = compCol[2];
-        var midHue = (hue + compHue)/2;
-        var midSat = (sat + compSat)/2;
-        var midLgh = (lgh + compLgh)/2;
-        $("body").css("background-image", "linear-gradient(to bottom right, hsl(" + hue + ", " + sat + "%, " + lgh + "%), hsl("  + compHue + ", " + compSat + "%, " + compLgh + "%)");
-        if (isDarkColor(midHue, midSat, midLgh)) {
-            $("body").css("color", "white");
-        }
-        else {
-            $("body").css("color", "black");
-        }
+        setBackgroundColor(hue, sat, lgh);
         $("#viewCounter").html(newViews);
     });
 }
