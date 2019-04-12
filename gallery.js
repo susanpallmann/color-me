cf240ad3faf1250a43947b87983a1af41ee02050
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-04-12 12:58:33 +1000

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -6,10 +6,12 @@ window.onload = function() {
       $("#gallery div:last-child").remove();
     }
     var hsl = getHSLFromString(data.val().color);
+    var compCol = getComplimentaryColor(hsl[0], hsl[1], hsl[2]);
+    var compColStr = "hsl(" + compCol[0] + ", " + compCol[1] + "%, " + compCol[2] + "%)";
     var textCol = "black";
     if (isDarkColor(hsl[0], hsl[1], hsl[2])) {
       textCol = "white";
     }
-    $("#gallery").prepend("<div style='color: " + textCol + "; background-color: " + data.val().color + ";'>Color Me " + data.val().title + "</br><small>by " + data.val().creator + "</small></div>");
+    $("#gallery").prepend("<div style='color: " + textCol + "; background-image: linear-gradient(to bottom right, " + data.val().color + ", " + compColStr + ");'>Color Me " + data.val().title + "</br><small>by " + data.val().creator + "</small></div>");
   });
 };
