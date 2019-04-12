cebb0ef4014eeba65d9bdc662c75b5e03754155a
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-04-12 13:01:07 +1000

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -6,8 +6,11 @@ window.onload = function() {
       $("#gallery div:last-child").remove();
     }
     var hsl = getHSLFromString(data.val().color);
+    console.log(hsl);
     var compCol = getComplimentaryColor(hsl[0], hsl[1], hsl[2]);
+    console.log(compCol[0]);
     var compColStr = "hsl(" + compCol[0] + ", " + compCol[1] + "%, " + compCol[2] + "%)";
+    console.log(compColStr);
     var textCol = "black";
     if (isDarkColor(hsl[0], hsl[1], hsl[2])) {
       textCol = "white";
