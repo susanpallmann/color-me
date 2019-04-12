f211767030259d91eeeaa5027419197f11214f03
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-04-12 16:49:22 +1000

    Update creator.js

--- creator.js
+++ creator.js
@@ -1,6 +1,8 @@
-$("input.value_color").on('input', function() {
-				var colorHue = $("input#value_colorHue").val();
-        var colorSat = $("input#value_colorSat").val();
-        var colorLgh = $("input#value_colorLgh").val();
-        $("#display_color").css("background-color", "hsl(" + colorHue + ", " + colorSat + "%, " + colorLgh + "%)");
+$(document).ready(function() {
+	$("input.value_color").on('input', function() {
+		var colorHue = $("input#value_colorHue").val();
+        	var colorSat = $("input#value_colorSat").val();
+        	var colorLgh = $("input#value_colorLgh").val();
+        	$("#display_color").css("background-color", "hsl(" + colorHue + ", " + colorSat + "%, " + colorLgh + "%)");
+	});
 });
