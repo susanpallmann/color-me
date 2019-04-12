cbc62e503521e7a023d880b125bf10e13567e3cf
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-04-12 16:50:48 +1000

    Update creator.js

--- creator.js
+++ creator.js
@@ -3,6 +3,11 @@ $(document).ready(function() {
 		var colorHue = $("input#value_colorHue").val();
         	var colorSat = $("input#value_colorSat").val();
         	var colorLgh = $("input#value_colorLgh").val();
+		var compColor = getComplementaryColor(colorHue, colorSat, colorLgh);
+		var compHue = compColor[0];
+		var compSat = compColor[1];
+		var compLgh = compColor[2];
         	$("#display_color").css("background-color", "hsl(" + colorHue + ", " + colorSat + "%, " + colorLgh + "%)");
+		$("#display_colorComp").css("background-color", "hsl(" + compHue + ", " + compSat + "%, " + compLgh + "%)");
 	});
 });
