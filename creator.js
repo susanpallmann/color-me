ab83c58d64d161dc51fb003f65c7dadd80ae812e
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-04-12 20:45:28 +1000

    Update creator.js

--- creator.js
+++ creator.js
@@ -7,6 +7,16 @@ $(document).ready(function() {
 		var compHue = compColor[0];
 		var compSat = compColor[1];
 		var compLgh = compColor[2];
+		var midHue = Math.round((colorHue + compHue)/2);
+		var midSat = Math.round((colorSat + compSat)/2);
+		var midLgh = Math.round((colorLgh + compLgh)/2);
+		if (isDarkColor(midHue, midSat, midLgh)) {
+			$("#display_color").css("color", "white");
+			$("#display_colorComp").css("color", "white");
+		} else {
+			$("#display_color").css("color", "black");
+			$("#display_colorComp").css("color", "black");
+		}
         	$("#display_color").css("background-color", "hsl(" + colorHue + ", " + colorSat + "%, " + colorLgh + "%)");
 		$("#display_colorComp").css("background-color", "hsl(" + compHue + ", " + compSat + "%, " + compLgh + "%)");
 	});
