4589af871a794089f08f11753d1a175396edf6c6
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-04-12 15:02:56 +1000

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -17,4 +17,32 @@ window.onload = function() {
     }
     $("#gallery").prepend("<div style='color: " + textCol + "; background-image: linear-gradient(to bottom right, " + hslString + ", " + compColStr + ");'>Color Me " + data.val().title + "</br><small>by " + data.val().creator + "</small></div>");
   });
+  
+  //Random BG color gen //
+	// Generate any random color from all possible HSL values
+	var randomNum = Math.random();
+	var hue = Math.round(randomNum * 360);
+	var sat = Math.round(Math.random()*100);
+	var lgh = Math.round(Math.random()*100);
+	
+	// Gets complementary color's HSL values
+	var compCol = getComplementaryColor(hue, sat, lgh);
+	var compHue = compCol[0];
+	var compSat = compCol[1];
+	var compLgh = compCol[2];
+	
+	// Gets intermediate HSL values between two colors
+	var midHue = (hue + compHue)/2;
+	var midSat = (sat + compSat)/2;
+	var midLgh = (lgh + compLgh)/2;
+	
+	// set background to gradient of random color and its complement
+	$("body").css("background-image", "linear-gradient(to bottom right, hsl(" + hue + ", " + sat + "%, " + lgh + "%), hsl("  + compHue + ", " + compSat + "%, " + compLgh + "%)");
+	
+	// set text color to white if colors are dark, or black if colors are light
+	if (isDarkColor(midHue, midSat, midLgh)) {
+		$("body").css("color", "white");
+	} else {
+		$("body").css("color", "black");
+	};
 };
