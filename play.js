b4c1256bb7370b39190ed9913b254740604e46a6
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-04-12 14:35:40 +1000

    Update play.js

--- play.js
+++ play.js
@@ -11,13 +11,20 @@ function loadPerspective(perspID) {
     firebase.database().ref('perspectives/' + perspID).once('value').then(function(snapshot) {
         var title = snapshot.child('title').val();
         var creator = snapshot.child('creator').val();
-        var colorHue = snapshot.child('colorHue').val();
-        var colorSat = snapshot.child('colorSat').val();
-        var colorLgh = snapshot.child('colorLgh').val();
+        var hue = snapshot.child('colorHue').val();
+        var sat = snapshot.child('colorSat').val();
+        var lgh = snapshot.child('colorLgh').val();
         var views = snapshot.child('views').val();
+        firebase.database().ref('perspectives/' + perspID).set({views: views + 1});
+        alert("This experience has " + views + 1 + " views!");
+        var compCol = getComplementaryColor(hue, sat, lgh);
+        var compHue = compCol[0];
+        var compSat = compCol[1];
+        var compLgh = compCol[2];
+        $("body").css("background-image", "linear-gradient(to bottom right, hsl(" + hue + ", " + sat + "%, " + lgh + "%), hsl("  + compHue + ", " + compSat + "%, " + compLgh + "%)");
+        $("#viewCounter").html(views + 1);
+        alert("This experience has " + views + 1 + " views!");
     });
-    firebase.database().ref('perspectives/' + perspID).set({views: views + 1});
-    alert("This experience has " + views + 1 + " views!");
 }
 
 window.onload = function() {
