b0cef894b2dfbf2d316b9ef3f216379988a2abfb
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-04-12 15:17:53 +1000

    Update play.js

--- play.js
+++ play.js
@@ -15,9 +15,8 @@ function loadPerspective(perspID) {
         var sat = snapshot.child('colorSat').val();
         var lgh = snapshot.child('colorLgh').val();
         var views = snapshot.child('views').val();
-        var newViews = Number(views + 2);
+        var newViews = Number(views + 1);
         firebase.database().ref('perspectives/' + perspID).child('views').set(newViews);
-        alert(hue, sat, lgh);
         setBackgroundColor(hue, sat, lgh);
         $("#viewCounter").html(newViews);
     });
