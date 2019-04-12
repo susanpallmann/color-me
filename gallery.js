20f5da0475b00a1fde4b2b177aa59c9e752fbd5c
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-04-12 11:59:24 +1000

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -10,6 +10,6 @@ window.onload = function() {
     if (isDarkColor(hsl[0], hsl[1], hsl[2])) {
       textCol = "white";
     }
-    $("#gallery").prepend("<div style='color: " + textCol + "; background-color: " + data.val().color + "'>Color Me " + data.val().title + "</br><small>by " + data.val().creator + "</small></div>");
+    $("#gallery").prepend("<div style='color: " + textCol + "; background-color: " + data.val().color + ";'>Color Me " + data.val().title + "</br><small>by " + data.val().creator + "</small></div>");
   });
 };
