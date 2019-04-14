b22e507d65d69a5f5dd2448f742e1997909c4a36
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-04-14 18:03:17 +1000

    Update play.js

--- play.js
+++ play.js
@@ -75,10 +75,10 @@ window.onload = function() {
     $("#stage_2").css("top", 0);
   });
   $("#stage_1 #mug").click(function() {
-    $(this).css("transition", "transform 4s ease-in-out");
+    $(this).css("transition", "transform 4s ease-in");
     var timer = setInterval(function() {
       $("#stage_1 #mug").css("transform", "skew(" + (Math.random()*90-45) + "deg, " + (Math.random()*90-45) + "deg)");
-    }, 4000);
+    }, 3000);
   });
   $("#stage_2 .nextButton").click(function() {
     $("#stage_2").css("top", "-100vh");
