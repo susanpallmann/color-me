d44cd264924ed8c6dc45a2ab14ba168a12608f1d
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-04-16 21:31:28 +1000

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -29,7 +29,8 @@ $(document).ready(function() {
 	$("#searchTerm").on("input", function() {
 		var searchTerm = $("#searchTerm").val();
 		var newQueryRef = firebase.database().ref('perspectives/visible').orderByChild('title').startAt(searchTerm).endAt(searchTerm + 'zzzzzzzzzzzzzzz').limitToFirst(maxThumbs);
-		if (searchTerm.length <= 3) {
+		alert(searchTerm);
+		if (searchTerm.length >= 3) {
 			loadGallery(newQueryRef);
 		}
 	});
