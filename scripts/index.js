afee18735f9b88cb79dc2bbf46fda9986f45eb5e
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-04-16 21:15:03 +1000

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 initialMax = 5;
 maxThumbs = initialMax;
 galleryCap = 30;
-queryRef = firebase.database().ref('perspectives/visible').orderByChild('hue').limitToFirst(maxThumbs);
+queryRef = firebase.database().ref('perspectives/visible').orderByChild('colorHue').limitToFirst(maxThumbs);
 galleryHTML = "";
 
 $(document).ready(function() {
@@ -38,7 +38,7 @@ $(document).ready(function() {
 
 function loadGallery() {
 	queryRef.off();
-	queryRef = firebase.database().ref('perspectives/visible').orderByChild('visibleInGallery').equalTo(true).limitToLast(maxThumbs);
+	queryRef = firebase.database().ref('perspectives/visible').orderByChild('colorHue').limitToFirst(maxThumbs);
 	queryRef.on('child_added', function(data) {
     		if ($("#gallery div").length >= maxThumbs) {
       			$("#gallery div:last-child").remove();
