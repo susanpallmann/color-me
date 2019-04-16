dd65f1a329988dc0f225f8cb404d622f74fd14b8
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-04-16 12:20:51 +1000

    Update shared.js

--- scripts/shared.js
+++ scripts/shared.js
@@ -179,7 +179,7 @@ function setBackgroundColor(hue, sat, lgh) {
 	var midLgh = (Number(lgh) + Number(compLgh))/2;
 	
 	// set background to gradient of random color and its complement
-	$("body").css("background-image", "radial-gradient(at bottom right, hsl(" + hue + ", " + sat + "%, " + lgh + "%), hsl("  + compHue + ", " + compSat + "%, " + compLgh + "%)");
+	$("body").css("background-image", "radial-gradient(at bottom right, hsl(" + compHue + ", " + compSat + "%, " + compLgh + "%), hsl("  + hue + ", " + sat + "%, " + lgh + "%)");
 	
 	// set text color to white if colors are dark, or black if colors are light
 	if (isDarkColor(midHue, midSat, midLgh)) {
