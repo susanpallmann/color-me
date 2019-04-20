6c60dd0ca126c5a8a2e53cb7d27e2c4b9ab3de53
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2019-04-20 20:06:03 +1000

    Update experience.js

--- scripts/experience.js
+++ scripts/experience.js
@@ -154,11 +154,13 @@ window.onload = function() {
   });
   
   $(window).bind('mousewheel', function(event) {
-    if (event.originalEvent.wheelDelta > 0 && stage > 0) {
-        goToStage(stage - 1);
+    if (event.originalEvent.wheelDelta > 0 && stage > 0 && Date.now() > lastScrollTime + 1000) {
+      lastScrollTime = Date.now();
+      goToStage(stage - 1);
     }
-    else if (event.originalEvent.wheelDelta < 0 && stage < 4) {
-        goToStage(stage + 1);
+    else if (event.originalEvent.wheelDelta < 0 && stage < 4 && Date.now() > lastScrollTime + 1000) {
+      lastScrollTime = Date.now();
+      goToStage(stage + 1);
     }
   });
   $(window).bind('DOMMouseScroll', function(e) {
